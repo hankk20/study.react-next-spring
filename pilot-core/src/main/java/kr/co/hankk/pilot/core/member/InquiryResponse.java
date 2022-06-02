6c8dab3d6aa3717635f7d08c196600bc2a909ddd
한광수 <hankk20@naver.com>
2022-06-02 23:34:21 +0900

    getter 추가

--- pilot-core/src/main/java/kr/co/hankk/pilot/core/member/InquiryResponse.java
+++ pilot-core/src/main/java/kr/co/hankk/pilot/core/member/InquiryResponse.java
@@ -2,9 +2,11 @@ package kr.co.hankk.pilot.core.member;
 
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public class InquiryResponse {
 
     @Builder.Default
